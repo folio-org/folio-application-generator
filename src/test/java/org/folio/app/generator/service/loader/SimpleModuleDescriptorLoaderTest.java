6463eb09a7c0351593c75ddb208ed9ef74d2bcd6
Kevin Day <kday@tamu.edu>
2025-03-19 14:24:34 -0500

    Provide "simple" registry type.
    Provide the Simple registry type that is designed to operate without a true OKAPI, such as a static GitHub pages website.
    
    The simple design only performs a fetch of the exact JSON.
    The remote server is expected to provide the `-latest` version because this simple type does not perform any complex versioning logic.
    
    Add `spring-boot-autoconfigure` because the `AnyNestedCondition` needs to be added to support condition on OR.
    This OR conditions allows for selecting either the `OkapiCondition` or the `SimpleCondition` as an `HttpCondition`.
    The `HttpClient` bean now only needs a single `@Conditional` to handle either the OKAPI or the Simple registry types.
    I am open to better solutions.
    
    Add `spring-boot-starter-test` to enable the use of `ReflectionTestUtils`.
    The `ReflectionTestUtils` is used to mock the `httpClient` during testing.
    
    ## Additional Notes
    
    The README table doesn't need the `|` at the end.
    Simplify this to avoid having to add a lot of spaces by removing the ending `|`.
    The `|` can be added if requested.

---
+++ src/test/java/org/folio/app/generator/service/loader/SimpleModuleDescriptorLoaderTest.java
@@ -0,0 +1,122 @@
+package org.folio.app.generator.service.loader;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.maven.plugin.logging.Log;
+import org.folio.app.generator.model.ModuleDefinition;
+import org.folio.app.generator.model.registry.SimpleModuleRegistry;
+import org.folio.app.generator.model.types.RegistryType;
+import org.folio.app.generator.support.UnitTest;
+import org.folio.app.generator.utils.JsonConverter;
+import org.json.JSONException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.testcontainers.shaded.org.apache.commons.io.IOUtils;
+
+@UnitTest
+@ExtendWith(MockitoExtension.class)
+class SimpleModuleDescriptorLoaderTest {
+
+  @InjectMocks private SimpleModuleDescriptorLoader loader;
+  @Mock private Log log;
+  @Mock private HttpClient httpClient;
+  @Mock private HttpResponse<Object> httpResponse;
+  @Mock private JsonConverter jsonConverter;
+
+  @AfterEach
+  void tearDown() {
+    verifyNoMoreInteractions(log, jsonConverter);
+  }
+
+  @Test
+  void getType_positive() {
+    assertThat(loader.getType()).isEqualTo(RegistryType.SIMPLE);
+  }
+
+  @Test
+  void findModuleDescriptor_positive_emptyResult() throws IOException, InterruptedException, JSONException {
+    ReflectionTestUtils.setField(loader, "httpClient", httpClient);
+
+    when(httpClient.send(any(HttpRequest.class), any())).thenReturn(httpResponse);
+    when(httpResponse.statusCode()).thenReturn(200);
+    when(httpResponse.statusCode()).thenReturn(200);
+
+    when(httpResponse.body()).thenReturn(IOUtils.toInputStream("", "UTF-8"));
+    when(jsonConverter.parse(any(InputStream.class), any())).thenReturn(new HashMap<String, Object>());
+
+    var result = loader.findModuleDescriptor(simpleRegistry(), fooModule("1.0.0"));
+
+    assertThat(result).isEmpty();
+    verify(log).warn("Module descriptor 'mod-foo-1.0.0' is not found in http://localhost/mod-foo-1.0.0");
+  }
+
+  @Test
+  void findModuleDescriptor_positive_singleModuleDescriptor() throws IOException, InterruptedException, JSONException {
+    ReflectionTestUtils.setField(loader, "httpClient", httpClient);
+
+    when(httpClient.send(any(HttpRequest.class), any())).thenReturn(httpResponse);
+    when(httpResponse.statusCode()).thenReturn(200);
+    when(httpResponse.statusCode()).thenReturn(200);
+
+    var expectedModuleDescriptor = fooModuleDescriptor("1.0.0");
+
+    when(httpResponse.body()).thenReturn(IOUtils.toInputStream("", "UTF-8"));
+    when(jsonConverter.parse(any(InputStream.class), any())).thenReturn(expectedModuleDescriptor);
+
+    var result = loader.findModuleDescriptor(simpleRegistry(), fooModule("1.0.0"));
+
+    assertThat(result).contains(expectedModuleDescriptor);
+    verify(log).info("Module descriptor 'mod-foo-1.0.0' loaded from http://localhost/mod-foo-1.0.0");
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = { 204, 404 })
+  void findModuleDescriptor_negative_loadFail(int statusCode) throws IOException, InterruptedException {
+    ReflectionTestUtils.setField(loader, "httpClient", httpClient);
+
+    when(httpClient.send(any(HttpRequest.class), any())).thenReturn(httpResponse);
+    when(httpResponse.statusCode()).thenReturn(statusCode);
+    when(httpResponse.statusCode()).thenReturn(statusCode);
+
+    var result = loader.findModuleDescriptor(simpleRegistry(), fooModule("1.0.0"));
+
+    assertThat(result).isEmpty();
+    verify(log).warn("Failed to load module descriptor 'mod-foo-1.0.0' from http://localhost/mod-foo-1.0.0: " + statusCode);
+  }
+
+  private static Map<String, Object> fooModuleDescriptor(String version) {
+    return Map.of(
+      "id", "mod-foo" + "-" + version,
+      "name", "Test name for module: " + "mod-foo",
+      "description", "A description for module: " + "mod-foo"
+    );
+  }
+
+  private static ModuleDefinition fooModule(String version) {
+    return new ModuleDefinition().id("mod-foo-" + version).name("mod-foo").version(version);
+  }
+
+  private static SimpleModuleRegistry simpleRegistry() {
+    return new SimpleModuleRegistry()
+      .url("http://localhost/")
+      .withGeneratedFields();
+  }
+}
