a4bdddbdb8e28427cfe45d5bb84c63291a575597
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-11-11 22:48:12 +0200

    APPDESCRIP-47. Add PreReleaseFilter enum for module dependency options

---
+++ src/main/java/org/folio/app/generator/model/PreReleaseFilter.java
@@ -0,0 +1,50 @@
+package org.folio.app.generator.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * Pre-release filter options for module dependencies.
+ */
+public enum PreReleaseFilter {
+  /**
+   * Include only pre-release versions.
+   */
+  ONLY("only"),
+
+  /**
+   * Include pre-release versions along with stable versions.
+   */
+  TRUE("true"),
+
+  /**
+   * Exclude pre-release versions (only stable versions).
+   */
+  FALSE("false");
+
+  private final String value;
+
+  PreReleaseFilter(String value) {
+    this.value = value;
+  }
+
+  @JsonValue
+  public String getValue() {
+    return value;
+  }
+
+  @JsonCreator
+  public static PreReleaseFilter fromValue(String value) {
+    if (value == null) {
+      return null;
+    }
+    for (PreReleaseFilter filter : PreReleaseFilter.values()) {
+      if (filter.value.equalsIgnoreCase(value)) {
+        return filter;
+      }
+    }
+    throw new IllegalArgumentException(
+      "Invalid preRelease value: '" + value + "'. Allowed values: 'only', 'true', 'false'"
+    );
+  }
+}
