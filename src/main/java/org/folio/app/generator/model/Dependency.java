e6415c6d617b65d5401ce383a90ac6ec48563477
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-11-16 13:49:42 +0200

    APPDESCRIP-47. Add Lombok @Builder annotation to Dependency class for improved object creation

--- src/main/java/org/folio/app/generator/model/Dependency.java
+++ src/main/java/org/folio/app/generator/model/Dependency.java
@@ -1,12 +1,14 @@
 package org.folio.app.generator.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Dependency {
 
   /**
