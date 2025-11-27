cf6aab05c0965939ad72ddaef3f5bc3855da4542
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-11-27 03:17:21 +0200

    APPDESCRIP-52. Refactor UpdateGenerator to handle output based on ModuleUrlsMode and write resolved application descriptor

--- src/main/java/org/folio/app/generator/UpdateGenerator.java
+++ src/main/java/org/folio/app/generator/UpdateGenerator.java
@@ -9,9 +9,12 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.folio.app.generator.configuration.ApplicationContextBuilder;
 import org.folio.app.generator.model.ApplicationDescriptor;
+import org.folio.app.generator.model.ResolvedApplicationDescriptor;
+import org.folio.app.generator.model.types.ModuleUrlsMode;
 import org.folio.app.generator.service.ApplicationDescriptorUpdateService;
 import org.folio.app.generator.service.JsonProvider;
 import org.folio.app.generator.service.ModuleRegistryProvider;
+import org.folio.app.generator.utils.PluginConfig;
 
 @Mojo(name = "updateFromJson", defaultPhase = LifecyclePhase.COMPILE, requiresDependencyResolution = RUNTIME)
 public class UpdateGenerator extends AbstractGeneratorMojo {
@@ -38,6 +41,25 @@ public class UpdateGenerator extends AbstractGeneratorMojo {
     var application = jsonProvider.readJsonFromFile(appDescriptorPath, ApplicationDescriptor.class, false);
 
     var descriptorUpdateService = ctx.getBean(ApplicationDescriptorUpdateService.class);
-    descriptorUpdateService.update(application, cmdModulesString, cmdUiModulesString);
+    var resolved = descriptorUpdateService.update(application, cmdModulesString, cmdUiModulesString);
+
+    var pluginConfig = ctx.getBean(PluginConfig.class);
+    writeOutput(resolved, jsonProvider, pluginConfig.getModuleUrlsMode());
+  }
+
+  private void writeOutput(ResolvedApplicationDescriptor resolved, JsonProvider jsonProvider, ModuleUrlsMode mode)
+    throws MojoExecutionException {
+    var outputDir = mavenProject.getBuild().getDirectory();
+
+    if (mode.needFullDescriptor()) {
+      jsonProvider.writeApplication(resolved.toFullDescriptor(), outputDir);
+    }
+
+    if (mode.needDescriptorUrl()) {
+      var filename = mode.needBoth()
+        ? resolved.getId() + ".url.json"
+        : resolved.getId() + ".json";
+      jsonProvider.writeApplication(resolved.toUrlOnlyDescriptor(), outputDir, filename);
+    }
   }
 }
