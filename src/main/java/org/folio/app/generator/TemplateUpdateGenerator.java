6a88a8de4a6d42b3f53850c0631e4ee0abfb25bc
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-11-28 14:21:00 +0200

    APPDESCRIP-51. Add TemplateUpdateGenerator for updating application descriptors based on the template

---
+++ src/main/java/org/folio/app/generator/TemplateUpdateGenerator.java
@@ -0,0 +1,66 @@
+package org.folio.app.generator;
+
+import static java.util.Optional.ofNullable;
+import static org.apache.maven.plugins.annotations.ResolutionScope.RUNTIME;
+import static org.folio.app.generator.utils.PluginUtils.emptyIfNull;
+
+import javax.inject.Inject;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.folio.app.generator.configuration.ApplicationContextBuilder;
+import org.folio.app.generator.model.ApplicationDescriptor;
+import org.folio.app.generator.model.ApplicationDescriptorTemplate;
+import org.folio.app.generator.model.UpdateConfig;
+import org.folio.app.generator.service.ApplicationDescriptorUpdateService;
+import org.folio.app.generator.service.JsonProvider;
+import org.folio.app.generator.service.ModuleRegistryProvider;
+
+@Mojo(name = "updateFromTemplate", defaultPhase = LifecyclePhase.COMPILE, requiresDependencyResolution = RUNTIME)
+public class TemplateUpdateGenerator extends AbstractGeneratorMojo {
+
+  @Parameter(name = "appDescriptorPath", defaultValue = "${basedir}/application-descriptor.json")
+  String appDescriptorPath;
+
+  @Parameter(name = "templatePath", defaultValue = "${basedir}/application-template.json")
+  String templatePath;
+
+  @Parameter(name = "allowDowngrade", defaultValue = "true")
+  boolean allowDowngrade;
+
+  @Parameter(name = "allowAddModules", defaultValue = "true")
+  boolean allowAddModules;
+
+  @Parameter(name = "removeUnlistedModules", defaultValue = "true")
+  boolean removeUnlistedModules;
+
+  @Inject
+  public TemplateUpdateGenerator(ModuleRegistryProvider registryProvider, ApplicationContextBuilder contextBuilder) {
+    super(registryProvider, contextBuilder);
+  }
+
+  @Override
+  public void execute() throws MojoExecutionException {
+    var ctx = buildApplicationContext();
+
+    var jsonProvider = ctx.getBean(JsonProvider.class);
+    var application = jsonProvider.readJsonFromFile(appDescriptorPath, ApplicationDescriptor.class, false);
+    var template = jsonProvider.readJsonFromFile(templatePath, ApplicationDescriptorTemplate.class, true);
+
+    application.setDependencies(
+      ofNullable(template.getDependencies()).orElse(application.getDependencies()));
+
+    var updateConfig = UpdateConfig.builder()
+      .allowDowngrade(allowDowngrade)
+      .allowAddModules(allowAddModules)
+      .removeUnlistedModules(removeUnlistedModules)
+      .build();
+
+    var updateService = ctx.getBean(ApplicationDescriptorUpdateService.class);
+    updateService.update(application,
+      emptyIfNull(template.getModules()),
+      emptyIfNull(template.getUiModules()),
+      updateConfig);
+  }
+}
