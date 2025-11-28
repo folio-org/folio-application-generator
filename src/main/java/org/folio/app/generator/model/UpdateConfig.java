4f3abc4c0a3576f586f42ace6a7944c1a7d30c55
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-11-28 14:19:14 +0200

    APPDESCRIP-51. Add UpdateConfig class for managing update configurations with default settings

---
+++ src/main/java/org/folio/app/generator/model/UpdateConfig.java
@@ -0,0 +1,22 @@
+package org.folio.app.generator.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UpdateConfig {
+
+  @Builder.Default
+  private final boolean allowDowngrade = false;
+
+  @Builder.Default
+  private final boolean allowAddModules = false;
+
+  @Builder.Default
+  private final boolean removeUnlistedModules = false;
+
+  public static UpdateConfig defaults() {
+    return UpdateConfig.builder().build();
+  }
+}
