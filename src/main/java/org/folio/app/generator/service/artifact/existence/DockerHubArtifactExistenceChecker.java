07549b61338f6d1453f6385fdbbe5454f04c9393
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-11-30 16:26:08 +0200

    APPDESCRIP-54. Add DockerHubArtifactExistenceChecker class to verify the existence of Docker images with HTTP checks

---
+++ src/main/java/org/folio/app/generator/service/artifact/existence/DockerHubArtifactExistenceChecker.java
@@ -0,0 +1,57 @@
+package org.folio.app.generator.service.artifact.existence;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.time.Duration;
+import lombok.SneakyThrows;
+import org.apache.maven.plugin.logging.Log;
+import org.folio.app.generator.model.ModuleDefinition;
+import org.folio.app.generator.model.registry.artifact.ArtifactRegistry;
+import org.folio.app.generator.model.types.ModuleType;
+import org.folio.app.generator.utils.JsonConverter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DockerHubArtifactExistenceChecker extends HttpArtifactExistenceChecker {
+
+  private static final int SUCCESS_STATUS_CODE = 200;
+
+  public DockerHubArtifactExistenceChecker(HttpClient httpClient, Log log, JsonConverter jsonConverter) {
+    super(httpClient, log, jsonConverter);
+  }
+
+  @Override
+  public ModuleType getModuleType() {
+    return ModuleType.BE;
+  }
+
+  @Override
+  @SneakyThrows
+  public boolean exists(ModuleDefinition module, ArtifactRegistry registry) {
+    var url = buildUrl(registry.getBaseUrl(), registry.getNamespace(), module.getName(), module.getVersion());
+    log.debug("Checking Docker image existence: " + url);
+
+    var request = HttpRequest.newBuilder()
+      .uri(URI.create(url))
+      .timeout(Duration.ofMinutes(1))
+      .GET()
+      .build();
+
+    var response = retryLoad(request);
+    var statusCode = response.statusCode();
+
+    if (statusCode == SUCCESS_STATUS_CODE) {
+      log.debug("Docker image found: " + module.getName() + ":" + module.getVersion());
+      return true;
+    }
+
+    log.debug("Docker image not found: " + module.getName() + ":" + module.getVersion()
+      + " (status: " + statusCode + ")");
+    return false;
+  }
+
+  private static String buildUrl(String baseUrl, String namespace, String imageName, String version) {
+    return cleanUrl(baseUrl) + "/" + namespace + "/" + imageName + "/tags/" + version;
+  }
+}
