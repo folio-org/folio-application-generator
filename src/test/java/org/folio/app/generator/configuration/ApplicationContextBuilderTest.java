a8872a80bea5de35c661bb1f1e57ffa21ba383f1
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-11-30 16:31:49 +0200

    APPDESCRIP-54. Add unit tests for ApplicationContextBuilder to validate artifact properties

---
+++ src/test/java/org/folio/app/generator/configuration/ApplicationContextBuilderTest.java
@@ -0,0 +1,101 @@
+package org.folio.app.generator.configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import org.folio.app.generator.model.registry.ModuleRegistries;
+import org.folio.app.generator.model.registry.OkapiModuleRegistry;
+import org.folio.app.generator.model.types.ArtifactRegistryType;
+import org.folio.app.generator.model.types.RegistryType;
+import org.folio.app.generator.support.UnitTest;
+import org.folio.app.generator.utils.PluginConfig;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@UnitTest
+@ExtendWith(MockitoExtension.class)
+class ApplicationContextBuilderTest {
+
+  @Mock private PluginConfig pluginConfig;
+
+  private AnnotationConfigApplicationContext context;
+
+  @BeforeEach
+  void setUp() {
+    context = new AnnotationConfigApplicationContext();
+  }
+
+  @AfterEach
+  void tearDown() {
+    var properties = context.getEnvironment().getSystemProperties();
+    for (var artifactRegistryType : ArtifactRegistryType.values()) {
+      properties.remove(artifactRegistryType.getPropertyName());
+    }
+    for (var registryType : RegistryType.values()) {
+      properties.remove(registryType.getPropertyName());
+    }
+    context.close();
+  }
+
+  @Test
+  void setSpringContextProperties_positive_validateArtifactsEnabled() {
+    when(pluginConfig.isValidateArtifacts()).thenReturn(true);
+    var moduleRegistries = new ModuleRegistries(List.of(), List.of());
+
+    var builder = new ApplicationContextBuilder()
+      .withPluginConfig(pluginConfig)
+      .withModuleRegistries(moduleRegistries);
+
+    builder.setSpringContextProperties(context);
+
+    var properties = context.getEnvironment().getSystemProperties();
+    for (var artifactRegistryType : ArtifactRegistryType.values()) {
+      assertThat(properties.get(artifactRegistryType.getPropertyName()))
+        .as("Property %s should be enabled", artifactRegistryType.getPropertyName())
+        .isEqualTo(true);
+    }
+  }
+
+  @Test
+  void setSpringContextProperties_positive_validateArtifactsDisabled() {
+    when(pluginConfig.isValidateArtifacts()).thenReturn(false);
+    var moduleRegistries = new ModuleRegistries(List.of(), List.of());
+
+    var builder = new ApplicationContextBuilder()
+      .withPluginConfig(pluginConfig)
+      .withModuleRegistries(moduleRegistries);
+
+    builder.setSpringContextProperties(context);
+
+    var properties = context.getEnvironment().getSystemProperties();
+    for (var artifactRegistryType : ArtifactRegistryType.values()) {
+      assertThat(properties.get(artifactRegistryType.getPropertyName()))
+        .as("Property %s should not be set", artifactRegistryType.getPropertyName())
+        .isNull();
+    }
+  }
+
+  @Test
+  void setSpringContextProperties_positive_withModuleRegistries() {
+    when(pluginConfig.isValidateArtifacts()).thenReturn(false);
+    var okapiRegistry = new OkapiModuleRegistry().url("http://localhost:9130");
+    var moduleRegistries = new ModuleRegistries(List.of(okapiRegistry), List.of());
+
+    var builder = new ApplicationContextBuilder()
+      .withPluginConfig(pluginConfig)
+      .withModuleRegistries(moduleRegistries);
+
+    builder.setSpringContextProperties(context);
+
+    var properties = context.getEnvironment().getSystemProperties();
+    assertThat(properties.get(RegistryType.OKAPI.getPropertyName()))
+      .as("Okapi registry property should be enabled")
+      .isEqualTo(true);
+  }
+}
