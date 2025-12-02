33a9a40d78e4662f96f7c9ef3cb005f90c52b226
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-12-02 19:36:05 +0200

    APPDESCRIP-54. Refactor DockerHubArtifactRegistry and FolioNpmArtifactRegistry to use static type constants

--- src/main/java/org/folio/app/generator/model/registry/artifact/DockerHubArtifactRegistry.java
+++ src/main/java/org/folio/app/generator/model/registry/artifact/DockerHubArtifactRegistry.java
@@ -1,18 +1,21 @@
 package org.folio.app.generator.model.registry.artifact;
 
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import org.folio.app.generator.model.types.ArtifactRegistryType;
 
-@Getter
 @EqualsAndHashCode(callSuper = true)
 public class DockerHubArtifactRegistry extends AbstractArtifactRegistry<DockerHubArtifactRegistry> {
 
   public static final String DEFAULT_BASE_URL = "https://hub.docker.com/v2/repositories";
 
-  private final ArtifactRegistryType type = ArtifactRegistryType.DOCKER_HUB;
+  private static final ArtifactRegistryType TYPE = ArtifactRegistryType.DOCKER_HUB;
 
   public DockerHubArtifactRegistry() {
     super(DEFAULT_BASE_URL);
+  }
+
+  @Override
+  public ArtifactRegistryType getType() {
+    return TYPE;
   }
 }
--- src/main/java/org/folio/app/generator/model/registry/artifact/FolioNpmArtifactRegistry.java
+++ src/main/java/org/folio/app/generator/model/registry/artifact/FolioNpmArtifactRegistry.java
@@ -1,18 +1,21 @@
 package org.folio.app.generator.model.registry.artifact;
 
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import org.folio.app.generator.model.types.ArtifactRegistryType;
 
-@Getter
 @EqualsAndHashCode(callSuper = true)
 public class FolioNpmArtifactRegistry extends AbstractArtifactRegistry<FolioNpmArtifactRegistry> {
 
   public static final String DEFAULT_BASE_URL = "https://repository.folio.org/repository";
 
-  private final ArtifactRegistryType type = ArtifactRegistryType.FOLIO_NPM;
+  private static final ArtifactRegistryType TYPE = ArtifactRegistryType.FOLIO_NPM;
 
   public FolioNpmArtifactRegistry() {
     super(DEFAULT_BASE_URL);
+  }
+
+  @Override
+  public ArtifactRegistryType getType() {
+    return TYPE;
   }
 }
