2fde62ce4443d3f0f119d789b14a32d02873c86d
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-12-02 18:23:30 +0200

    APPDESCRIP-54. Implement AbstractArtifactRegistry and refactor DockerHub and FolioNpm artifact registries to extend it

--- src/main/java/org/folio/app/generator/model/registry/artifact/FolioNpmArtifactRegistry.java
+++ src/main/java/org/folio/app/generator/model/registry/artifact/FolioNpmArtifactRegistry.java
@@ -1,50 +1,18 @@
 package org.folio.app.generator.model.registry.artifact;
 
-import static org.apache.commons.lang3.StringUtils.isBlank;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import org.folio.app.generator.model.types.ArtifactRegistryType;
 
-@Data
-public class FolioNpmArtifactRegistry implements ArtifactRegistry {
+@Getter
+@EqualsAndHashCode(callSuper = true)
+public class FolioNpmArtifactRegistry extends AbstractArtifactRegistry<FolioNpmArtifactRegistry> {
 
   public static final String DEFAULT_BASE_URL = "https://repository.folio.org/repository";
 
-  @Setter(AccessLevel.NONE)
-  private ArtifactRegistryType type = ArtifactRegistryType.FOLIO_NPM;
+  private final ArtifactRegistryType type = ArtifactRegistryType.FOLIO_NPM;
 
-  private String baseUrl = DEFAULT_BASE_URL;
-  private String namespace;
-
-  public FolioNpmArtifactRegistry baseUrl(String baseUrl) {
-    this.baseUrl = baseUrl;
-    return this;
-  }
-
-  public FolioNpmArtifactRegistry namespace(String namespace) {
-    this.namespace = namespace;
-    return this;
-  }
-
-  @Override
-  public boolean isValid() {
-    if (isBlank(namespace)) {
-      return false;
-    }
-
-    if (isBlank(baseUrl)) {
-      return false;
-    }
-
-    try {
-      new URL(baseUrl);
-      return true;
-    } catch (MalformedURLException e) {
-      return false;
-    }
+  public FolioNpmArtifactRegistry() {
+    super(DEFAULT_BASE_URL);
   }
 }
