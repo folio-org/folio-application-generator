45c367841fd87fb82440b9915b36d6af39323544
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-12-03 18:24:07 +0200

    EUREKASUP-111. Add JsonIgnoreProperties annotation to ApplicationDescriptorTemplate

--- src/main/java/org/folio/app/generator/model/ApplicationDescriptorTemplate.java
+++ src/main/java/org/folio/app/generator/model/ApplicationDescriptorTemplate.java
@@ -1,10 +1,12 @@
 package org.folio.app.generator.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ApplicationDescriptorTemplate {
 
   /**
