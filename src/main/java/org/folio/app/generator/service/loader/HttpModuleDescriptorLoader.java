c0c82a834723ea6dfcb48c6d6aa211adab83a52f
epam-avramenko <Vasyl_Avramenko@epam.com>
2026-01-07 20:45:38 +0200

    APPDESCRIP-67. Add retry mechanism with enhanced handling for network errors and exponential backoff in `HttpModuleDescriptorLoader`

--- src/main/java/org/folio/app/generator/service/loader/HttpModuleDescriptorLoader.java
+++ src/main/java/org/folio/app/generator/service/loader/HttpModuleDescriptorLoader.java
@@ -1,6 +1,9 @@
 package org.folio.app.generator.service.loader;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -15,8 +18,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public abstract class HttpModuleDescriptorLoader implements ModuleDescriptorLoader {
 
-  protected static final Set<Integer> RETRYABLE_STATUS_CODES = Set.of(504);
+  protected static final Set<Integer> RETRYABLE_STATUS_CODES = Set.of(429, 502, 503, 504);
   protected static final int RETRYABLE_ATTEMPTS_NUMBER = 5;
+  protected static final long RETRY_DELAY_MS = 1000;
 
   protected final HttpClient httpClient;
   protected final Log log;
@@ -25,11 +29,28 @@ public abstract class HttpModuleDescriptorLoader implements ModuleDescriptorLoad
   @SneakyThrows
   protected HttpResponse<InputStream> retryLoad(HttpRequest request) {
     var attemptsCount = 0;
-    var response = httpClient.send(request, HttpResponse.BodyHandlers.ofInputStream());
-    while (RETRYABLE_STATUS_CODES.contains(response.statusCode()) && attemptsCount++ < RETRYABLE_ATTEMPTS_NUMBER) {
-      response = httpClient.send(request, HttpResponse.BodyHandlers.ofInputStream());
+    IOException lastException = null;
+
+    while (attemptsCount < RETRYABLE_ATTEMPTS_NUMBER) {
+      try {
+        var response = httpClient.send(request, HttpResponse.BodyHandlers.ofInputStream());
+        if (!RETRYABLE_STATUS_CODES.contains(response.statusCode())) {
+          return response;
+        }
+        log.debug("Retrying request due to status code " + response.statusCode()
+          + " (attempt " + (attemptsCount + 1) + ")");
+      } catch (SocketException | SocketTimeoutException e) {
+        lastException = e;
+        log.warn("Network error, retrying (attempt " + (attemptsCount + 1) + "): " + e.getMessage());
+      }
+      attemptsCount++;
+      Thread.sleep(RETRY_DELAY_MS * attemptsCount);
     }
-    return response;
+
+    if (lastException != null) {
+      throw lastException;
+    }
+    return httpClient.send(request, HttpResponse.BodyHandlers.ofInputStream());
   }
 
   protected static String cleanUrl(String url) {
