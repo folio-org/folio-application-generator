1a0ecdd454562cc5b8dd88fb6bdde9bc509eacf7
epam-avramenko <Vasyl_Avramenko@epam.com>
2026-01-07 20:45:29 +0200

    APPDESCRIP-67. Add exception handling and exponential backoff to `HttpArtifactExistenceChecker` retry mechanism

--- src/main/java/org/folio/app/generator/service/artifact/existence/HttpArtifactExistenceChecker.java
+++ src/main/java/org/folio/app/generator/service/artifact/existence/HttpArtifactExistenceChecker.java
@@ -1,5 +1,8 @@
 package org.folio.app.generator.service.artifact.existence;
 
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -24,13 +27,28 @@ public abstract class HttpArtifactExistenceChecker implements ArtifactExistenceC
   @SneakyThrows
   protected <T> HttpResponse<T> retryLoad(HttpRequest request, BodyHandler<T> bodyHandler) {
     var attemptsCount = 0;
-    var response = httpClient.send(request, bodyHandler);
-    while (RETRYABLE_STATUS_CODES.contains(response.statusCode()) && attemptsCount++ < RETRYABLE_ATTEMPTS_NUMBER) {
-      log.debug("Retrying request due to status code " + response.statusCode() + " (attempt " + attemptsCount + ")");
-      Thread.sleep(RETRY_DELAY_MS);
-      response = httpClient.send(request, bodyHandler);
+    IOException lastException = null;
+
+    while (attemptsCount < RETRYABLE_ATTEMPTS_NUMBER) {
+      try {
+        var response = httpClient.send(request, bodyHandler);
+        if (!RETRYABLE_STATUS_CODES.contains(response.statusCode())) {
+          return response;
+        }
+        log.debug("Retrying request due to status code " + response.statusCode()
+          + " (attempt " + (attemptsCount + 1) + ")");
+      } catch (SocketException | SocketTimeoutException e) {
+        lastException = e;
+        log.warn("Network error, retrying (attempt " + (attemptsCount + 1) + "): " + e.getMessage());
+      }
+      attemptsCount++;
+      Thread.sleep(RETRY_DELAY_MS * attemptsCount);
     }
-    return response;
+
+    if (lastException != null) {
+      throw lastException;
+    }
+    return httpClient.send(request, bodyHandler);
   }
 
   protected static String cleanUrl(String url) {
