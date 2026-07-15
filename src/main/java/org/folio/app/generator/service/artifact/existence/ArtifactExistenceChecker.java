078cd78719c290ad43f3b4070eba63d037bcf12f
epam-avramenko <Vasyl_Avramenko@epam.com>
2026-07-15 14:45:05 +0300

    APPDESCRIP-71. Switch from ModuleType to ArtifactRegistryType in ArtifactExistenceChecker interface

--- src/main/java/org/folio/app/generator/service/artifact/existence/ArtifactExistenceChecker.java
+++ src/main/java/org/folio/app/generator/service/artifact/existence/ArtifactExistenceChecker.java
@@ -2,11 +2,11 @@ package org.folio.app.generator.service.artifact.existence;
 
 import org.folio.app.generator.model.ModuleDefinition;
 import org.folio.app.generator.model.registry.artifact.ArtifactRegistry;
-import org.folio.app.generator.model.types.ModuleType;
+import org.folio.app.generator.model.types.ArtifactRegistryType;
 
 public interface ArtifactExistenceChecker {
 
-  ModuleType getModuleType();
+  ArtifactRegistryType getRegistryType();
 
   boolean exists(ModuleDefinition module, ArtifactRegistry registry);
 }
