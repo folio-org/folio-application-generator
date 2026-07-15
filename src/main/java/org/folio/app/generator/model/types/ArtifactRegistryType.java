a03e6c0f099248653373a8b20ddd8a86b852bfb7
epam-avramenko <Vasyl_Avramenko@epam.com>
2026-07-15 13:38:08 +0300

    APPDESCRIP-71. Add AWS_ECR support to ArtifactRegistryType enum

--- src/main/java/org/folio/app/generator/model/types/ArtifactRegistryType.java
+++ src/main/java/org/folio/app/generator/model/types/ArtifactRegistryType.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum ArtifactRegistryType {
 
   DOCKER_HUB("docker-hub", "folio-app-generator.docker-hub.enabled"),
-  FOLIO_NPM("folio-npm", "folio-app-generator.folio-npm.enabled");
+  FOLIO_NPM("folio-npm", "folio-app-generator.folio-npm.enabled"),
+  AWS_ECR("aws-ecr", "folio-app-generator.aws-ecr.enabled");
 
   private final String value;
   private final String propertyName;
